dc7de47775f50b036eedd11098021db75abe1be1
Krunali <krunalilole29@gmail.com>
2022-07-08 21:55:51 +0530

    Added Spring AOP Project to learn the concepts of using AOP in project to manage the cross cutting concerns

---
+++ springaop/springaop/src/main/java/com/bridgelabz/springaop/SpringaopApplication.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.springaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringaopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringaopApplication.class, args);
+		System.out.println("Welcome to AOP Spring Project");
+	}
+
+}
